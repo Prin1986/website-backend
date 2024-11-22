043acb1b0bbe23535f3064862bf3af6548ea8081
unknown <eseogheneaziakpono@gmail.com>
2024-11-22 06:09:32 -0800

    all code since backend class started

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+//31/10/2024
+//operator
+//arithmetic operators
+$x =10;
+$y =5;
+echo $x + $y;
+echo "<br>";
+echo $x -$y;
+echo "<br>";
+echo $x * $y;
+echo "<br>";
+echo $x / $y;
+echo "<br>";
+echo $x % $y;
+echo "<br>";
+echo $x ** $y;
+echo"<br>";
+//increment and decrement operatorse
+$x++;
+echo $x;
+echo "<br>";
+
+$x--;
+echo $x;
+$a =100;
+$b =50;
+// additional assignment
+$a += $b;
+echo "<br>";
+echo $a;
+//subtrantion assignment
+$a -= $b;
+echo "<br>";
+echo $a;
+//logical operators
+//examples of logical operators are:
+echo "<br>";
+    if ($a ==100 && $b == 50)
+     {
+        echo "we are done.";
+    }
+
+    echo "<br>";
+    if ($a ==100 || $b == 55)
+     {
+        echo "we are done.";
+    }
+
+    echo "<br>";
+    if ($a ==100 xor $b == 55)
+     {
+        echo "we are done.";
+    }
+
+    echo "<br>";
+    if (!($a ==101))
+     {
+        echo "we are done.";
+    }
+
+    // 11/4/2024
+    //PHP conditional statements
+   // four conditional notable statement in php viz
+
+  // if statement
+ //  if else statement
+   //else if statement
+   //switch statement
+    // if statement
+    //the if statement is one of the most important in php
+    //example:
+    //_______
+    // variable declaration\
+
+$domesticanimal = array ('dog', 'cat','horse', 'monkey', 'cow',);
+$cities = ['asaba', 'delta', 'lagos','sapele',]
+
+echo "<br> $domesticanimal [0]";
+
+// function
+
+function greetMe {
+
